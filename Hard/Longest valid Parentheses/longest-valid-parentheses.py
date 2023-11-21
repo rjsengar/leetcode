bae4fef43e9a610f9443d259755ec574926d08ce
Raj Pratap Singh Sengar <119128767+rjsengar@users.noreply.github.com>
2023-11-21 21:59:47 +0530

    Added Solution - GfG to GitHub

---
+++ Hard/Longest valid Parentheses/longest-valid-parentheses.py
@@ -0,0 +1,33 @@
+# User function Template for Python3
+
+# User function Template for Python3
+
+class Solution:
+    def maxLength(self, s):
+        a=[]
+        a.append(-1)
+        ans=0
+        for i in range(len(s)):
+            #print(a[-1])
+            if s[i]=='(':
+                a.append(i)
+            else:
+                a.pop()
+                if len(a)==0:
+                    a.append(i)
+                ans=max(ans,i-a[-1])
+        return ans
+
+
+#{ 
+ # Driver Code Starts
+# Initial Template for Python3
+
+if __name__ == '__main__':
+    t = int(input())
+    for _ in range(t):
+        S = input()
+        
+        ob = Solution()
+        print(ob.maxLength(S))
+# } Driver Code Ends
