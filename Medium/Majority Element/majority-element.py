45d28692c7635a62405a40d32c683c20eab2eb60
Raj Pratap Singh Sengar <119128767+rjsengar@users.noreply.github.com>
2023-11-27 20:26:57 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Majority Element/majority-element.py
@@ -0,0 +1,46 @@
+#User function template for Python 3
+
+class Solution:
+    def majorityElement(self, A, N):
+        d={}
+        for i in A:
+            if i in d:
+                d[i]+=1
+            else:
+                d[i]=1
+        for i in A:
+            if(d[i]>N/2):
+                return i
+        return -1
+
+
+
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+import math
+
+from sys import stdin
+
+
+def main():
+        T=int(input())
+        while(T>0):
+            
+            N=int(input())
+
+            A=[int(x) for x in input().strip().split()]
+            
+            
+            obj = Solution()
+            print(obj.majorityElement(A,N))
+            
+            T-=1
+
+
+if __name__ == "__main__":
+    main()
+# } Driver Code Ends
