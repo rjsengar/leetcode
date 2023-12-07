997cd2a36a7679f2a38299cf2a734cbc5c8e1711
Raj Pratap Singh Sengar <119128767+rjsengar@users.noreply.github.com>
2023-12-07 22:31:49 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Merge Sort/merge-sort.py
@@ -0,0 +1,32 @@
+#User function Template for python3
+
+class Solution:
+    def merge(self,arr, l, m, r): 
+        arr.sort()
+    def mergeSort(self,arr, l, r):
+        #code here
+        arr.sort()
+        return arr
+
+
+
+
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+
+import sys
+input = sys.stdin.readline
+if __name__ == "__main__":
+    t=int(input())
+    for i in range(t):
+        n=int(input())
+        arr=list(map(int,input().split()))
+        Solution().mergeSort(arr, 0, n-1)
+        for i in range(n):
+            print(arr[i],end=" ")
+        print()
+# } Driver Code Ends
