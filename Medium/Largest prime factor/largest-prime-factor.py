6285268dc45cef7c9abbe1e2de71c3966d06de30
Raj Pratap Singh Sengar <119128767+rjsengar@users.noreply.github.com>
2023-12-13 23:48:57 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Largest prime factor/largest-prime-factor.py
@@ -0,0 +1,30 @@
+#User function Template for python3
+
+class Solution:
+    def largestPrimeFactor (self, N):
+        num = 2
+        while ((num * num) <=N):
+            if (N % num == 0):
+                N = N//num 
+            else:
+                num+=1 
+        return int(N)
+        
+
+
+
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+if __name__ == '__main__': 
+    t = int (input ())
+    for _ in range (t):
+        N = int(input())
+       
+
+        ob = Solution()
+        print(ob.largestPrimeFactor(N))
+# } Driver Code Ends
