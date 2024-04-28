0df7e8fe6481d8b418ebee49289e46d097471c66
Raj Pratap Singh Sengar <119128767+rjsengar@users.noreply.github.com>
2024-04-28 11:32:46 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Delete Middle of Linked List/delete-middle-of-linked-list.py
@@ -0,0 +1,82 @@
+#User function Template for python3
+
+'''
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+'''
+class Solution:
+    def deleteMid(self,head):
+        '''
+        head:  head of given linkedList
+        return: head of resultant llist
+        '''
+        
+        #code here
+        c=0
+        #code here
+        curr1=head
+        curr2=head
+        h1=h2=Node(-1)
+        while(curr1):
+            c+=1
+            curr1=curr1.next
+        if c<=1:
+            return None
+        c=c//2
+        p=None
+        
+        for i in range(c):
+            p=curr2
+            curr2=curr2.next
+        p.next=curr2.next
+        return head
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+#contributed by RavinderSinghPB
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+
+class Llist:
+    def __init__(self):
+        self.head = None
+
+    def insert(self, data, tail):
+        node = Node(data)
+
+        if not self.head:
+            self.head = node
+            return node
+
+        tail.next = node
+        return node
+
+
+def printList(head):
+    while head:
+        print(head.data, end=' ')
+        head = head.next
+    print()
+
+if __name__ == '__main__':
+    t = int(input())
+
+    for tcs in range(t):
+        n=int(input())
+        arr1 = [int(x) for x in input().split()]
+        ll = Llist()
+        tail = None
+        for nodeData in arr1:
+            tail = ll.insert(nodeData, tail)
+        obj = Solution();
+        res=obj.deleteMid(ll.head)
+        printList(res)
+# } Driver Code Ends
