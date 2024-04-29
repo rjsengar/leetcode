5897421fdbc7ab8688d96fdeb2e918e8e3fe0d10
Raj Pratap Singh Sengar <119128767+rjsengar@users.noreply.github.com>
2024-04-29 20:35:22 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Remove every kth node/remove-every-kth-node.py
@@ -0,0 +1,66 @@
+#Your task is to complete this function
+#Your function should return the new head pointer
+'''
+class node:
+    def __init__(self,x):
+        self.data = x
+        self.next = None
+'''
+
+class Solution:
+    def deleteK(self, head, k):
+        curr=head
+        l=[]
+        while(curr):
+            l.append(curr.data)
+            curr=curr.next
+        c=h=node(-1)
+        for i in range(len(l)):
+            if (i+1)%k==0:
+                continue
+            else:
+                c.next=node(l[i])
+                c=c.next
+        return h.next
+
+
+#{ 
+ # Driver Code Starts
+class node:
+
+    def __init__(self, x):
+        self.data = x
+        self.next = None
+
+
+def createLinkedList(arr):
+    head = node(arr[0])
+    curr = head
+    for i in range(1, len(arr)):
+        new_node = node(arr[i])
+        curr.next = new_node
+        curr = curr.next
+
+    return head
+
+
+def printlist(ptr):
+    while ptr != None:
+        print(ptr.data, end=" ")
+        ptr = ptr.next
+    print()
+
+
+if __name__ == '__main__':
+    t = int(input())
+    for i in range(t):
+        n = int(input())
+        arr = list(map(int, input().strip().split()))
+        k = int(input())
+
+        obj = Solution()
+        head = createLinkedList(arr)
+        new_head = obj.deleteK(head, k)
+        printlist(new_head)
+
+# } Driver Code Ends
