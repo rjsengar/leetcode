cfc5eabc45b894b2a8e1e8a619457b9fdfcf9fd2
Raj Pratap Singh Sengar <119128767+rjsengar@users.noreply.github.com>
2024-05-03 22:44:49 +0530

    Updated Solution - GfG to GitHub

--- Easy/K distance from root/k-distance-from-root.py
+++ Easy/K distance from root/k-distance-from-root.py
@@ -9,25 +9,27 @@ class Node:
         self.left = None
         self.right = None
 '''
-def KDistance(root,k):
-    q=[root]
-    c=0
-    while(q):
-        l=len(q)
-        t=[]
-        while(l>0):
-            n=q[0]
-            q.remove(n)
-            t.append(n.data)
-            if n.left:
-                q.append(n.left)
-            if n.right:
-                q.append(n.right)
-            l-=1
-        if c==k:
-            return t
-        c+=1
-    return []
+class Solution:
+    def KDistance(self,root,k):
+        q=[root]
+        c=0
+        while(q):
+            l=len(q)
+            t=[]
+            while(l>0):
+                n=q[0]
+                q.remove(n)
+                t.append(n.data)
+                if n.left:
+                    q.append(n.left)
+                if n.right:
+                    q.append(n.right)
+                l-=1
+            if c==k:
+                return t
+            c+=1
+        return []
+
 
 
 #{ 
@@ -107,7 +109,8 @@ if __name__=="__main__":
         k = int(input())
         s=input()
         root=buildTree(s)
-        nodes = KDistance(root,k)
+        ob=Solution();
+        nodes = ob.KDistance(root,k)
         for node in nodes:
             print(node,end=' ')
         print()
