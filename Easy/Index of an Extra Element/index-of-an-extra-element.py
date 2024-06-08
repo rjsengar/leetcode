036537ff09da78fa03897b7f86f9969561b9880e
Raj Pratap Singh Sengar <119128767+rjsengar@users.noreply.github.com>
2024-06-08 23:56:16 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Index of an Extra Element/index-of-an-extra-element.py
@@ -0,0 +1,21 @@
+class Solution:
+    def findExtra(self,n,a,b):
+        for i in range(n-1):
+            if a[i]!=b[i]:
+                return i
+        return n-1
+
+        #add code here
+
+
+#{ 
+ # Driver Code Starts
+if __name__ == "__main__":
+    t = int(input())
+    for i in range(t):
+        n = int(input())
+        a = list(map(int, input().split()))
+        b = list(map(int, input().split()))
+        print(Solution().findExtra(n, a, b))
+
+# } Driver Code Ends
