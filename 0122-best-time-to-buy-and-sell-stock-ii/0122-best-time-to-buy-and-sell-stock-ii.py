c83d3fab9e4eba0c0f460fe91575dcfb54f77cb6
Raj Pratap Singh Sengar <119128767+rjsengar@users.noreply.github.com>
2024-08-02 15:51:13 +0530

    Time: 61 ms (36.55%), Space: 17.9 MB (40.95%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.py
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.py
@@ -5,6 +5,7 @@ class Solution:
         while(i<len(prices)-1):
             if prices[i]<prices[i+1]:
                 t=prices[i]
+                i+=1
                 while(i<len(prices)-1 and prices[i]<=prices[i+1]):
                     i+=1
                 c+=prices[i]-t
