106d07b7b3f7c71bbea150f9e854aae00759a95d
Raj Pratap Singh Sengar <119128767+rjsengar@users.noreply.github.com>
2024-08-15 15:20:13 +0530

    Time: 554 ms (5.11%), Space: 18.4 MB (5.19%) - LeetHub

--- 0095-unique-binary-search-trees-ii/0095-unique-binary-search-trees-ii.py
+++ 0095-unique-binary-search-trees-ii/0095-unique-binary-search-trees-ii.py
@@ -10,46 +10,51 @@ class Solution:
         l=[]
         l1=[]
         l3=[]
+        an=[]
         for i in range(1,n+1):
             l.append(i)
-        p=permutations(l)
-        for i in list(p):
-            i=list(i)
-            h=c=TreeNode(i[0])
-            k=i[1:]
-            for j in k:
-                c=h
-                while(1):
-                    if j<c.val:
-                        if c.left:
-                            c=c.left
-                        else:
-                            c.left=TreeNode(j)
-                            break
-                    elif j>c.val:
-                        if c.right:
-                            c=c.right
-                        else:
-                            c.right=TreeNode(j)
-                            break
-            ans=[]
-            q=[h]
-            while(q):
-                le=len(q)
-                while(le>0):
-                    n=q[0]
-                    q.remove(n)
-                    ans.append(n.val)
-                    if n.left:
-                        q.append(n.left)
-                    if n.right:
-                        q.append(n.right)
-                    le-=1
-            if ans not in l3:
-                l1.append(h)
-                l3.append(ans)
+        def permute(a, l, r,ans): 
+            if l == r:
+                i=a[:]
+                h=c=TreeNode(i[0])
+                k=i[1:]
+                for j in k:
+                    c=h
+                    while(1):
+                        if j<c.val:
+                            if c.left:
+                                c=c.left
+                            else:
+                                c.left=TreeNode(j)
+                                break
+                        elif j>c.val:
+                            if c.right:
+                                c=c.right
+                            else:
+                                c.right=TreeNode(j)
+                                break
+                an=[]
+                q=[h]
+                while(q):
+                    le=len(q)
+                    while(le>0):
+                        n=q[0]
+                        q.remove(n)
+                        an.append(n.val)
+                        if n.left:
+                            q.append(n.left)
+                        if n.right:
+                            q.append(n.right)
+                        le-=1
+                if an not in l3:
+                    l1.append(h)
+                    l3.append(an)
+            else: 
+                for i in range(l, r): 
+                    a[l], a[i] = a[i], a[l] 
+                    permute(a, l+1, r,ans) 
+                    a[l], a[i] = a[i], a[l]
+        permute(l,0,n,[])
             
-                        
-
         return l1
         
