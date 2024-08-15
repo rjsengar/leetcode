0ffa9553894cbf7aea82fcf15d07c0b52c57ce38
Raj Pratap Singh Sengar <119128767+rjsengar@users.noreply.github.com>
2024-08-15 14:51:37 +0530

    Time: 630 ms (27.14%), Space: 21.2 MB (58.06%) - LeetHub

--- 0890-lemonade-change/0890-lemonade-change.py
+++ 0890-lemonade-change/0890-lemonade-change.py
@@ -9,12 +9,12 @@ class Solution:
                 d[i]+=1
             elif i==10:
                 d[i]+=1
-                if d[5]>0:
+                if d[5]:
                     d[5]-=1
                 else:
                     return False
             elif i==20:
-                if d[10]>0 and d[5]>0:
+                if d[10] and d[5]:
                     d[10]-=1
                     d[5]-=1
                 elif d[5]>=3:
