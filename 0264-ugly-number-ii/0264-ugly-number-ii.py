75e3f29691b64a38bf6a82867f3a1f916b05152c
Raj Pratap Singh Sengar <119128767+rjsengar@users.noreply.github.com>
2024-08-18 13:39:59 +0530

    Time: 880 ms (5.61%), Space: 19 MB (5.66%) - LeetHub

---
+++ 0264-ugly-number-ii/0264-ugly-number-ii.py
@@ -0,0 +1,16 @@
+class Solution:
+    def nthUglyNumber(self, n: int) -> int:
+        d={}
+        l=[1]
+        d[1]=1
+        while(len(l)<n):
+            for i in l:
+                for j in [2,3,5]:
+                    if i*j not in d:
+                        d[i*j]=1
+                        l.append(i*j)
+                if len(l)>=10*n:
+                    break
+        l.sort()
+        return l[n-1]
+        
