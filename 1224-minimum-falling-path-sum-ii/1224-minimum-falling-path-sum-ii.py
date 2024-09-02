2e05ba765ba805c3303fdc98a0f9622ac2ce2452
Raj Pratap Singh Sengar <119128767+rjsengar@users.noreply.github.com>
2024-09-02 23:13:30 +0530

    Time: 4775 ms (23.03%), Space: 20 MB (83.72%) - LeetHub

---
+++ 1224-minimum-falling-path-sum-ii/1224-minimum-falling-path-sum-ii.py
@@ -0,0 +1,23 @@
+class Solution:
+    def minFallingPathSum(self, grid: List[List[int]]) -> int:
+        n=len(grid)
+        dp=[]
+        for i in range(n):
+            l1=[]
+            for j in range(n):
+                if i==0:
+                    l1.append(grid[i][j])
+                else:
+                    l1.append(0)
+            dp.append(l1)
+        for i in range(1,n):
+            for j in range(n):
+                m=10000000
+                for k in range(n):
+                    if k!=j:
+                        m=min(m,dp[i-1][k]+grid[i][j])
+                dp[i][j]=m
+        m1=100000
+        for i in range(n):
+            m1=min(m1,dp[n-1][i])
+        return m1
