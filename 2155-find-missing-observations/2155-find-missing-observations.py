0c1564c7de0ac95b5671b24d2a6caf523c9667bc
Raj Pratap Singh Sengar <119128767+rjsengar@users.noreply.github.com>
2024-09-05 14:59:43 +0530

    Time: 1065 ms (18.18%), Space: 26.8 MB (69.79%) - LeetHub

--- 2155-find-missing-observations/2155-find-missing-observations.py
+++ 2155-find-missing-observations/2155-find-missing-observations.py
@@ -1,14 +1,14 @@
 class Solution:
     def missingRolls(self, rolls: List[int], mean: int, n: int) -> List[int]:
-        l=[1]*n
-        s=sum(l)
+        
         s1=sum(rolls)
         t=len(rolls)+n
         k=(t*mean)-s1
-        print(k)
+        # print(k)
         if k>n*6 or k<n:
             return []
-        
+        l=[k//n]*n
+        s=sum(l)
         p=0
         while(s<k):
             if l[p]<6:
@@ -19,10 +19,5 @@ class Solution:
             if s==k:
                 return l
             p=p%n
-        # if (sum(l)+sum(rolls))//t==mean:
         return l
-        # return []
-                
-        # print(k)
-        return []
         
