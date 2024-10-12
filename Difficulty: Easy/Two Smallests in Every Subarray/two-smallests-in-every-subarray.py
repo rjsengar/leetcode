ed585793cad71482fd5f168500b8d6ec48c90e19
Raj Pratap Singh Sengar <119128767+rjsengar@users.noreply.github.com>
2024-10-12 14:42:30 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Two Smallests in Every Subarray/two-smallests-in-every-subarray.py
@@ -0,0 +1,25 @@
+class Solution:
+    def pairWithMaxSum(self, arr):
+        m=-1
+        for i in range(len(arr)-1):
+            m=max(m,arr[i]+arr[i+1])
+        return m
+
+
+#{ 
+ # Driver Code Starts
+if __name__ == "__main__":
+    import sys
+    input = sys.stdin.read
+    data = input().strip().split("\n")
+
+    t = int(data[0])
+    lines = data[1:]
+
+    for line in lines:
+        s = list(map(int, line.strip().split()))
+        solution = Solution()
+        res = solution.pairWithMaxSum(s)
+        print(res)
+
+# } Driver Code Ends
