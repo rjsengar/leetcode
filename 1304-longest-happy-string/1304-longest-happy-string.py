5a825b95ce046c57f6c87734fc034336c001cbca
Raj Pratap Singh Sengar <119128767+rjsengar@users.noreply.github.com>
2024-10-17 22:34:58 +0530

    Time: 0 ms (100%), Space: 16.7 MB (29.72%) - LeetHub

--- 1304-longest-happy-string/1304-longest-happy-string.py
+++ 1304-longest-happy-string/1304-longest-happy-string.py
@@ -1,4 +1,4 @@
-from queue import PriorityQueue
+
 class Solution:
     def longestDiverseString(self, a: int, b: int, c: int) -> str:
         l=[]
@@ -29,104 +29,7 @@ class Solution:
                 break
         return s
 
-
-        # while(len(l)>1):
-        #     if l[0][0]>=2:
-        #         s+=l[0][1]*2
-        #         l[0][0]-=2
-        #     else:
-        #         if l[0][0]==1:
-        #             s+=l[0][1]
-        #             l[0][0]-=1
-        #             l.pop(0)
-        #     if l[-1][0]>=2:
-        #         s+=l[-1][1]*2
-        #         l[-1][0]-=2
-        #     else:
-        #         if l[-1][0]==1:
-        #             s+=l[-1][1]
-        #             l.pop()
-        # for i in l:
-        #     if s[-1]!=i[1]:
-        #         if i[0]>=2:
-        #             s+=i[1]*2
-        #         else:
-        #             s+=i[1]
-        #             i[1]-=1
-            
-                
-                    
-        return s  
-            
-        # q=PriorityQueue()
-        for i in l:
-            q.put(i)
-        k=1
-        while(q.qsize()):
-            k=1
-            a1=q.get()
-            if abs(a[0])>=2:
-                s+=a[1]*2
-                a[0]=abs(a[0])-2
-            elif abs(a[0])==1:
-                s+=a[1]
-                k=0
-            b1=q.get()
-            if abs(b1[0])>=2:
-                s+=b1[1]*2
-                b1[0]=abs(b1[0])-2
-            elif abs(b1[0])==1:
-                s+=b1[1]
-                k=0
-
-
-  
-            break
-        return ""
         
-
-        # t1=1
-        # t2=1
-        # t3=1
-        # s=""
-        # while(1):
-        #     s1=s
-        #     if t1:
-        #         if l[0][0]>=2:
-        #             s+=l[0][1]*2
-        #             t1=0
-        #             t2=1
-        #             t3=1
-        #         else:
-        #             s+=l[0][1]
-        #             t1=0
-        #             t2=1
-        #             t3=1
-        #     elif t2:
-        #         if l[1][0]>=2:
-        #             s+=l[1][1]*2
-        #             t2=0
-        #             t1=1
-        #             t3=1
-        #         else:
-        #             s+=l[1][1]
-        #             t2=0
-        #             t1=1
-        #             t3=1
-        #     else:
-        #         if t3 and  l[2][0]>=2:
-        #             s+=l[2][1]*2
-        #             t3=0
-        #             t1=1
-        #             t2=1
-        #         elif t3:
-        #             s+=l[2][1]
-        #             t3=0
-        #             t1=1
-        #             t2=1
-        #     if s1==s:
-        #         break
-        # return s
 
         
         
