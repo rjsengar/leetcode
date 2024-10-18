4c66d7ad6eb292679e7cf5cd755346ccfef6c14f
Raj Pratap Singh Sengar <119128767+rjsengar@users.noreply.github.com>
2024-10-18 22:42:26 +0530

    Time: 655 ms (39.83%), Space: 26 MB (13.35%) - LeetHub

--- 2170-count-number-of-maximum-bitwise-or-subsets/2170-count-number-of-maximum-bitwise-or-subsets.py
+++ 2170-count-number-of-maximum-bitwise-or-subsets/2170-count-number-of-maximum-bitwise-or-subsets.py
@@ -5,7 +5,12 @@ class Solution:
             s|=i
         ans=[[]]
         for j in nums:
-            ans+=[i+[j] for i in ans]
+            l1=[]
+            for i in ans:
+                a = i+[j]
+                l1.append(a)
+            for k in l1:
+                ans.append(k)
         c=0
         for i in ans:
             s1=0
