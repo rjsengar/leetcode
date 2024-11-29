3cd4499bc2ec664f294db6ffdc2d2f6ef7cabbdb
Raj Pratap Singh Sengar <119128767+rjsengar@users.noreply.github.com>
2024-11-29 13:13:27 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Add Binary Strings/add-binary-strings.py
@@ -0,0 +1,25 @@
+#User function Template for python3
+class Solution:
+	def addBinary(self, s1, s2):
+		a=int(s1,2)
+		b=int(s2,2)
+		b=bin((a+b)).replace("0b","")
+		return b
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+if __name__ == '__main__':
+    T = int(input())
+    for i in range(T):
+        a = input().strip()
+        b = input().strip()
+        ob = Solution()
+        answer = ob.addBinary(a, b)
+
+        print(answer)
+        print("~")
+
+# } Driver Code Ends
