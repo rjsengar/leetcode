63f671ea871dc9d281550365b4bc434e1ee17ee6
Raj Pratap Singh Sengar <119128767+rjsengar@users.noreply.github.com>
2024-12-04 23:14:39 +0530

    Time: 1706 ms (20.81%), Space: 30.3 MB (26.21%) - LeetHub

--- 3594-identify-the-largest-outlier-in-an-array/3594-identify-the-largest-outlier-in-an-array.py
+++ 3594-identify-the-largest-outlier-in-an-array/3594-identify-the-largest-outlier-in-an-array.py
@@ -1,16 +1,14 @@
 class Solution:
     def getLargestOutlier(self, nums: List[int]) -> int:
-        nums.sort()
+        s=sum(nums)
+        m=min(nums)
         d={}
-        # print(nums)
-        n=len(nums)
         for i in range(len(nums)):
             if nums[i] not in d:
                 d[nums[i]]=1
             else:
                 d[nums[i]]+=1
-        s=sum(nums)
-        m=min(nums)
+        
         for i in range(len(nums)):
             s1=s
             s1-=nums[i]
@@ -18,9 +16,6 @@ class Solution:
             if s1%2==0 and (s1//2) in d and d[s1//2]>=1:
                 m=max(m,nums[i])
             d[nums[i]]+=1
-        return m
-            
-
         return m
                 
 
