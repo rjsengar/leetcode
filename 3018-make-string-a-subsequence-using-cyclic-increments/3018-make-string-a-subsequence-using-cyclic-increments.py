749a63217b0261d9abcfa1edd8d818961ff74a45
Raj Pratap Singh Sengar <119128767+rjsengar@users.noreply.github.com>
2024-12-04 15:42:10 +0530

    Time: 84 ms (44.44%), Space: 19.2 MB (5.69%) - LeetHub

--- 3018-make-string-a-subsequence-using-cyclic-increments/3018-make-string-a-subsequence-using-cyclic-increments.py
+++ 3018-make-string-a-subsequence-using-cyclic-increments/3018-make-string-a-subsequence-using-cyclic-increments.py
@@ -5,24 +5,22 @@ class Solution:
         c=0
         d={}
         d1={}
+        k=len(l1)-1
         s1='abcdefghijklmnopqrstuvwxyz'
         for i in range(len(s1)):
             d[s1[i]]=i
             d1[i]=s1[i]
         i=len(l2)-1
-        while(i>=0):
-            if l1 and l2[i]==l1[-1]:
+        while(i>=0 and k>=0):
+            if l2[i]==l1[k]:
                 c+=1
                 i-=1
-                l1.pop()
+                k-=1
                 continue
-            elif l1 and d1[(d[l1[-1]]+1)%26]==l2[i]:
+            elif d1[(d[l1[k]]+1)%26]==l2[i]:
                 c+=1
                 i-=1
-            if l1==[] and c!=len(l2):
-                return False
-            l1.pop()
-        print(c)
+            k-=1
         if c==len(l2):
             return True
         return False
