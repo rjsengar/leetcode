edb884a8105b5a0624b8a8c6d810bb53ae827576
Raj Pratap Singh Sengar <119128767+rjsengar@users.noreply.github.com>
2024-12-07 20:24:10 +0530

    Time: 0 ms (100%), Space: 17.5 MB (6.29%) - LeetHub

--- 0078-subsets/0078-subsets.py
+++ 0078-subsets/0078-subsets.py
@@ -4,9 +4,13 @@ class Solution:
         n=len(nums)
         def solve(i,s):
             if i>=n:
-                l1.append(s)
+                # print(s)
+                l1.append(s[:])           
                 return
-            solve(i+1,s+[nums[i]])
+            
+            s.append(nums[i])
+            solve(i+1,s)
+            s.pop()
             solve(i+1,s)
         solve(0,[])
         l1.sort()
